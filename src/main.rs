8dba5bc3451ed89d00e5c86f3b6244b5e4d1dd6d
Huskyto Gonar <huskygonar@gmail.com>
2025-01-22 15:11:23 -0500

    Base sync service

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use sevice::SyncService;
 mod model;
 mod sevice;
 mod util;
+mod config;
 
 fn main() {
     quick_test();
@@ -13,9 +14,19 @@ fn main() {
 
 fn quick_test() {
     let file_def = FileDefinition::new("Filename01".to_string(), "Path02".to_string());
-    let new_id = SyncService::create_empty(file_def).expect("Create empty failed");
+    let file_def = SyncService::create_empty(&file_def).expect("Create empty failed");
+    let new_id = file_def.id.as_ref().expect("Id was not updated");
     println!("New id: {new_id}");
 
+    let is_updated = SyncService::update_file(new_id, "tmp/test-file.txt").expect("Error updating file");
+    println!("Is updated? {is_updated}");
 
+    let file_data = SyncService::get_file(&file_def).expect("Failed to get file data!");
+    println!("Retrieved data: {}", &String::from_utf8(file_data).unwrap());
 
+    let is_deleted = SyncService::delete_file(new_id).expect("Failed to delete file!");
+    println!("Is deleted? {is_deleted}");
+
+    let did_panic = std::panic::catch_unwind(|| SyncService::get_file(&file_def).expect("Getting deleted failed!")).is_err();
+    println!("Did retrieve non existant panic? {did_panic}");
 }
--- src/sevice.rs
+++ src/sevice.rs
@@ -5,7 +5,7 @@ use crate::{model::FileDefinition, util::Util};
 
 pub struct SyncService;
 impl SyncService {
-    pub fn create_empty(fd: FileDefinition) -> Result<String, String> {
+    pub fn create_empty(fd: &FileDefinition) -> Result<FileDefinition, String> {
         let url = Util::build_url("api/v1/file");
         let client = reqwest::blocking::Client::new();
         let js = serde_json::to_string(&fd).expect("Serialization error.");
@@ -15,8 +15,12 @@ impl SyncService {
                 .send() {
             Ok(res) => {
                 if res.status() == 201 {
-                    let new_id = res.headers().get("location").expect("Location not present");
-                    Ok(new_id.to_str().unwrap().to_string())
+                    let new_id = res.headers().get("location")
+                            .expect("Location not present")
+                            .to_str().unwrap().to_string();
+                    let mut new_fd = fd.clone();
+                    new_fd.id = Some(new_id);
+                    Ok(new_fd)
                 }
                 else {
                     Err("Wrong status".to_string())
@@ -26,5 +30,61 @@ impl SyncService {
         }
     }
 
+    pub fn update_file(file_id: &str, local_path: &str) -> Result<bool, String> {
+        let content = std::fs::read(local_path).expect("Failed to read local file");
+        let url = Util::build_url(&format!("api/v1/file/{file_id}"));
+        let client = reqwest::blocking::Client::new();
+        match client.put(url)
+                .body(content)
+                .send() {
+            Ok(res) => {
+                if res.status() == 202 {
+                    Ok(true)
+                }
+                else {
+                    Err("Wrong status".to_string())
+                }
+            },
+            Err(e) => Err(e.to_string()),
+        }
+
+
+    }
+
+    pub fn get_file(file_def: &FileDefinition)  -> Result<Vec<u8>, String> {
+        let file_id = file_def.id.as_ref().expect("Missing file id!");
+        let url = Util::build_url(&format!("api/v1/file/{}", file_id));
+        let client = reqwest::blocking::Client::new();
+        match client.get(url)
+                .send() {
+            Ok(res) => {
+                if res.status() == 200 {
+                    let body = res.bytes().expect("Erro getting bytes").to_vec();
+                    Ok(body)
+                }
+                else {
+                    Err("Wrong status".to_string())
+                }
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
+
+    pub fn delete_file(file_id: &str) -> Result<bool, String> {
+        let url = Util::build_url(&format!("api/v1/file/{}", file_id));
+        let client = reqwest::blocking::Client::new();
+        match client.delete(url)
+                .send() {
+            Ok(res) => {
+                if res.status() == 202 {
+                    Ok(true)
+                }
+                else {
+                    Err("Wrong status".to_string())
+                }
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
 
 }
