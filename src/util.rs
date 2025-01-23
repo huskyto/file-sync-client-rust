4553915ba1eb593c800a66df0ee8743fd2b00bc8
Huskyto Gonar <huskygonar@gmail.com>
2025-01-23 00:14:41 -0500

    Remove root from path

--- src/util.rs
+++ src/util.rs
@@ -25,7 +25,10 @@ impl Util {
         std::fs::read(local_path).expect("Failed to read local file")
     }
     pub fn get_local_file_definition(full_path: &str) -> Option<FileDefinition> {
-        let (path, name) = Self::split_full_path(full_path);
+        let stripped_path = Path::new(full_path)
+                .strip_prefix(Config::get_base_path())
+                .unwrap().to_str().unwrap();
+        let (path, name) = Self::split_full_path(stripped_path);
         let content = Self::read_file_content(full_path);
         let checksum = Self::checksum(&content);
         let last_update = std::fs::metadata(full_path).expect("Failed to retrieve file metadata")
